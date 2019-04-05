a44e2cf03fdf8a787b897c2719ba4ab08a76df1b
Helder Bacanhim <helderperestrelo99@gmail.com>
2019-04-05 22:00:38 +0100

    vn
    n

---
+++ ficha20/components/app-quote.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <slots></slots>
+    </div>
+</template>
+<style>
+div{
+    background-color: white;
+    box-shadow: 0 0 24px rgba(0, 0, 0, 0.3);
+    padding: 3rem;
+    width: 420px;
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+</style>
