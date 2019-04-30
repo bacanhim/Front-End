7e0bab6d11a806980a4355cbccbe35f55a7e4307
Helder Bacanhim <helderperestrelo99@gmail.com>
2019-04-30 21:32:45 +0100

    dfs

---
+++ ficha_tema4/bla/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-toolbar fixed color="primary" dark>
+
+    </v-toolbar>
+
+    <main>
+      <router-view />
+    </main>
+  </v-app>
+</template>
+<script>
+</script>
